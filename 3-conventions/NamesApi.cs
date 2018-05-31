11e0e10699ce5af512c888d64592bc140dff4f8b
Fanie Reynders <fanie@reynders.co>
2018-05-31 07:12:03 +0200

    region

--- 3-conventions/NamesApi.cs
+++ 3-conventions/NamesApi.cs
@@ -4,12 +4,14 @@ namespace Api.People
 {
     public class NamesApi
     {
+        #region ctor
         private readonly IPeopleRepository people;
 
         public NamesApi(IPeopleRepository people)
         {
             this.people = people;
         }
+        #endregion
         
         public string[] Get() => people.All;
     }
